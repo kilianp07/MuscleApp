71490a6d9327f45fff22be0c66977f018048ffea
kilianp07 <kilianp07@icloud.com>
2023-02-10 12:29:29 +0100

    removed firebase, added auth0 and bugsnag bugtracking

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+var Bugsnag = require('@bugsnag/js')
+var BugsnagPluginExpress = require('@bugsnag/plugin-express')
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const { expressjwt: expressJwt } = require('express-jwt');
+const jwks = require('jwks-rsa');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const port = process.env.PORT || 50000;
+
+Bugsnag.start({
+  apiKey: '2ba623624a33a894a56ece104eb75f75',
+  plugins: [BugsnagPluginExpress]
+})
+var middleware = Bugsnag.getPlugin('express')
+app.use(middleware.requestHandler)
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +36,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var jwtCheck = expressJwt({
+  secret: jwks.expressJwtSecret({
+    cache : true,
+    ratelimit : true,
+    jwksRequestsPerMinute : 5,
+    jwksUri : "https://dev-aa5pftowvhf5q3p2.us.auth0.com/.well-known/jwks.json"
+  }),
+  audience : "muscleapp",
+  issuer : "https://dev-aa5pftowvhf5q3p2.us.auth0.com/",
+  algorithms : ['RS256']
+})
+
+app.use(jwtCheck);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +67,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.listen(port);
+app.use(middleware.errorHandler)
 
 module.exports = app;
