fd3bc6ce1e8467e536a023ee1866a4d1874639ea
kilianp07 <57315153+kilianp07@users.noreply.github.com>
2023-03-03 11:55:18 +0100

    add sequelize models (#9)
    * add sequelize models
    
    * add models
    
    * Add relations

---
+++ models/bodymember.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class BodyMember extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      BodyMember.hasMany(models.Muscle)
+      BodyMember.hasMany(models.Exercise)
+    }
+  }
+  BodyMember.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'BodyMember',
+  });
+  return BodyMember;
+};
