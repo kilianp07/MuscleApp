6fc0504920087b268add8eebe9d63caf4a6b404e
kilianp07 <57315153+kilianp07@users.noreply.github.com>
2023-06-15 13:43:42 +0200

    add exercises routes (#44)

--- handlers/exercise/exercise.go
+++ handlers/exercise/exercise.go
@@ -1,6 +1,8 @@
 package exerciseHandler
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kilianp07/MuscleApp/database/controller"
 	exerciseModel "github.com/kilianp07/MuscleApp/models/Exercise"
@@ -45,6 +47,129 @@ func (handler *ExerciseHandler) CreateExercise(c *gin.Context) {
 	}
 
 	if err = handler.controller.CreateExercise(exercise); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, exerciseModel.ModelToPublic(exercise))
+}
+
+func (handler *ExerciseHandler) GetExerciseByID(c *gin.Context) {
+	var (
+		exercise *exerciseModel.Public
+		err      error
+	)
+
+	id := c.Param("id")
+	idint, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	exercise, err = handler.controller.GetExerciseByID(idint)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, exercise)
+}
+
+func (handler *ExerciseHandler) GetSomeExercises(c *gin.Context) {
+	var (
+		exercises []exerciseModel.Public
+		err       error
+	)
+
+	number := c.Param("number")
+	numberint, err := strconv.Atoi(number)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	exercises, err = handler.controller.GetSomeExercises(numberint)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, exercises)
+}
+
+func (handler *ExerciseHandler) UpdateExercise(c *gin.Context) {
+	var (
+		data     exerciseModel.Public
+		exercise *exerciseModel.Exercise
+		err      error
+	)
+
+	id := c.Param("id")
+	idint, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = gincontext.GetUserId(c)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	if err = c.ShouldBindJSON(&data); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = handler.controller.GetExerciseByID(idint)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	exercise.Title = data.Title
+	exercise.Description = data.Description
+	exercise.Image = data.Image
+	exercise.Video = data.Video
+	exercise.ID = uint(idint)
+
+	if err = handler.controller.UpdateExercise(exercise); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, exerciseModel.ModelToPublic(exercise))
+}
+
+func (handler *ExerciseHandler) DeleteExercise(c *gin.Context) {
+	var (
+		exercise *exerciseModel.Exercise
+		err      error
+	)
+
+	id := c.Param("id")
+	idint, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = gincontext.GetUserId(c)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = handler.controller.GetExerciseByID(idint)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	exercise.ID = uint(idint)
+
+	if err = handler.controller.DeleteExercise(exercise); err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
